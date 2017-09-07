dfd69fccf1dcc261b9d0a2e437cbc139c41d2a16
Bignegrod <30939945+Bignegrod@users.noreply.github.com>
2017-09-06 22:38:11 -0300

    Add files via upload

---
+++ teatro/Abonado.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teatro;
+
+
+public class Abonado {
+    private String rut;
+    private String nombreCom;
+    private int celular;
+    private String email;
+    private char tipo;
+    private boolean activo;
+      public Abonado() {
+        this.rut = " ";
+        this.nombreCom = " ";
+        this.celular = 0;
+        this.email = " ";
+        this.tipo = ' ';
+        this.activo=true;
+    }
+    
+    
+    
+    public Abonado(String rut, String nombreCom, int celular, String email, char tipo, boolean activo) {
+        setRut(rut);
+        setNombreCom  (nombreCom);
+        setCelular  (celular);
+        setEmail  (email);
+        setTipo ( tipo);
+        setActivo(activo);
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        if(rut.trim().length()>=10)
+            
+        this.rut = rut;
+        else
+            System.out.println("Error:Ingrese un rut correcto   ");
+    }
+
+    public String getNombreCom() {
+        return nombreCom;
+    }
+
+    public void setNombreCom(String nombreCom) {
+        this.nombreCom = nombreCom;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getTipo() {
+        return tipo;
+    }
+    
+    
+    public void setTipo(char tipo) {
+        if(tipo=='p' || tipo=='P' || tipo=='e' || tipo=='E')
+        this.tipo = tipo;
+        else
+            System.out.println("Error: Ingrese un tipo correcto");
+        
+        
+    }
+    public boolean isActivo(){
+        return activo;
+    }
+    public void setActivo(boolean activo){
+        
+        this.activo=activo;
+    }
+    
+    
+    
+    public void imprimirAbonado(){
+        
+        System.out.println("Rut:"+getRut());
+        System.out.println("Nombre Completo:"+getNombreCom());
+        System.out.println("Celular:"+getCelular());
+        System.out.println("Email:"+getEmail());
+        System.out.println("Tipo:"+getTipo());
+        
+    }
+            
+}
---
+++ teatro/Abono.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teatro;
+
+/**
+ *
+ * @author duoc
+ */
+public class Abono {
+    private String codigo ;
+    private int numeroButaca;
+    private int valorAs;
+    private int funciones;
+    private String fechaVigencia;
+    private Abonado abon;
+
+    public Abono() {
+        this.codigo = "";
+        this.numeroButaca = 0;
+        this.valorAs = 0;
+        this.funciones = 0;
+        this.fechaVigencia = "";
+        this.abon = new Abonado();//new asegura la colaboracion
+    }
+
+    public Abono(String codigo, int numeroButaca, int valorAs, int funciones, String fechaVigencia, Abonado abon) {
+        setCodigo (codigo);
+        setNumeroButaca  (numeroButaca);
+        setValorAs (valorAs);
+        setFunciones   (funciones);
+        setFechaVigencia  (fechaVigencia);
+        setAbon (abon);
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getNumeroButaca() {
+        return numeroButaca;
+    }
+
+    public void setNumeroButaca(int numeroButaca) {
+        if(numeroButaca>=1 && numeroButaca<=700)
+        this.numeroButaca = numeroButaca;
+        else
+            System.out.println("Error:Ingres eun numero valido  ");
+    }
+
+    public int getValorAs() {
+        return valorAs;
+    }
+
+    public void setValorAs(int valorAs) {
+        if(valorAs>200000)
+        this.valorAs = valorAs;
+        else
+            System.out.println("Error:Ingrese un Valor valido");
+    }
+
+    public int getFunciones() {
+        return funciones;
+    }
+
+    public void setFunciones(int funciones) {
+        if(funciones>5)
+        this.funciones = funciones;
+        else
+            System.out.println("Error");
+    }
+
+    public String getFechaVigencia() {
+        return fechaVigencia;
+    }
+
+    public void setFechaVigencia(String fechaVigencia) {
+        this.fechaVigencia = fechaVigencia;
+    }
+
+    public Abonado getAbon() {
+        return abon;
+    }
+
+    public void setAbon(Abonado abon) {
+        this.abon = abon;
+    }
+    
+    public void imprimirAbono(){
+        
+        System.out.println("Codigo:"+getCodigo());
+        System.out.println("Numero butaca:"+getNumeroButaca());
+        System.out.println("valorAs:"+getValorAs());
+        System.out.println("Funciones:"+getFunciones());
+        System.out.println("Fecha vigencia:"+getFechaVigencia());
+        getAbon().imprimirAbonado();
+        
+    }
+    
+    
+    public float descuentoAbonado(float desc){
+        float pagoFinal=0;
+        
+        pagoFinal=getValorAs()-getValorAs()*desc/100;
+        
+        return pagoFinal;
+        
+        //casteo return (int)pagoFinal;
+        //es forzar el float a int
+    }
+    
+    
+}
+

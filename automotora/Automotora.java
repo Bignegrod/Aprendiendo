f17bb6834a75788f6a4d7ceee377ee54311236da
Bignegrod <30939945+Bignegrod@users.noreply.github.com>
2017-09-15 15:08:13 -0400

    Update Automotora.java

--- automotora/Automotora.java
+++ automotora/Automotora.java
@@ -1,21 +1,73 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package automotora;
 
-/**
- *
- * @author duoc
- */
+import java.util.Scanner;
+
 public class Automotora {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner sc=new Scanner(System.in);
+        
+        
+        System.out.println("Ingrese nombre de la distribuidora");
+        String asa=sc.nextLine();
+        System.out.println("Ingrese numero");
+        int num=sc.nextInt();
+        
+        Distribuidor mm1=new Distribuidor(asa, num);
+                
+        
+        
+        
+        
+        
+        System.out.println("Ingrese tipo de vehiculo");
+      String tip=sc.next();
+        System.out.println("Ingrese patente");
+       String pat=sc.next();
+        System.out.println("¿El vehiculo es usado?");
+       boolean usa=sc.nextBoolean();
+       System.out.println("Ingrese el precio del vehiculo");
+       int pre=sc.nextInt();
+        System.out.println("Que transmision es el vehiculo");
+        char trans=sc.next().charAt(0);
+        System.out.println("Ingrese kilometraje del vehiculo");
+        int kil=sc.nextInt();
+       
+       Vehiculo imprimir=new Vehiculo(tip, pat, usa, pre, trans, kil,mm1); 
+       
+       imprimir.imprimirVehiculo(); //aqui mandai a imprimir los datos del vehiculo
+       
+    System.out.println("-----------------------------");     
+    String rr=imprimir.detalle(); // aqui es string pq la clase donde estan los detalles es string y estay guardando los datos en la variable rr
+        
+        System.out.println(rr);
+     System.out.println("-----------------------------");
+     
+        
+        
+
+        System.out.println("Ingrese % del iva");//aqui estoy preguntando por el iva
+        float iv=sc.nextFloat(); // y aqui lo guardo
+        
+        imprimir.precioVenta( iv);
+        
+        float nn=imprimir.precioVenta( iv); //aqui iniciai con float pq la clase precio venta es con float po wm y estay guardando los daots en la variable nn
+          System.out.println("-----------------------------");
+        System.out.println(nn);
+        /*
+        Mira pq me dio paja y no sabia que era el precio de utilidad aplique solo el descuento apra ver si el codigo compilaba bien 
+        en la otra clase vei po que le elimine utilkidad y todo para ver si corria bien
+        
+        
+        */
+          System.out.println("-----------------------------");
+          
+          int mm=imprimir.cantidadKilometraje();
+          
+          System.out.println(mm);
+           System.out.println("-----------------------------");
+    
     }
     
 }

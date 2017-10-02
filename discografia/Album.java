20d245c142fb0e2b08de8c52e0d0fc6260ff2a8d
Bignegrod <30939945+Bignegrod@users.noreply.github.com>
2017-10-02 16:53:16 -0400

    Update Album.java

--- discografia/Album.java
+++ discografia/Album.java
@@ -10,5 +10,34 @@ package discografia;
  * @author Negrod
  */
 public class Album {
+      public void agregarCancion (Cancion nombre){
+        
+    }
+    
+    public void listarCanciones (){
+       for(Cancion tema : Cancion.getNombre){
+            System.out.println("-------------------------------------");
+            System.out.println("Cancion: "+Cancion.getNombre());
+           System.out.println("Duracion"+Cancion.get.Duuracion());
+       }
+   } 
+}
+    
+    
+/*
+    public Album() {
+    
+    public boolean agregarCancion(Cancion can){
+        boolean encontrado=false;
+        
+        
+        for(Cancion tema:canci)
+        {
+            if(tema.)
+        }  
+          
+    }
+}*/
+
     
 }

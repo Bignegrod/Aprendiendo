5fe0d4a9212cb2669d92326a8cd5aa09c141e35b
Bignegrod <30939945+Bignegrod@users.noreply.github.com>
2017-10-11 22:27:39 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,20 @@
+package interfeis;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * Todos los metodos en las clases abstractas deben ser publicos, nunca van privados
+ */
+public interface Animal {
+    
+    public abstract void   avanzar ();
+    public abstract void comer (String comia);
+    
+    
+    
+}
